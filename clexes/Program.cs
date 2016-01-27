84462648bda2334765a36055640ebc869acbc42d
Lukas Hagman <Lukas.Hagman@Multisoft.se>
2016-01-27 11:59:38 +0100

    Initial commit

---
+++ clexes/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.IO;
+using System.Text;
+
+namespace clexes
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	class Cat
+	{
+		class CountingReader : StreamReader {
+			private int _lineNumber = 0;
+			public int LineNumber { get { return _lineNumber; } }
+
+			public CountingReader(Stream stream) : base(stream) { }
+
+			public override string ReadLine() {
+				_lineNumber++;
+				return base.ReadLine();
+			}
+		}
+
+		class F
+		{
+			public string Name { get; set; }
+			public string FullPath { get; set; }
+		}
+
+		static void Main(string[] args)
+		{
+			var arguments = new List<string>(args);
+			var switches = new List<string>(arguments.Where(a => a[0] == '-'));
+			arguments.RemoveAll(a => switches.Contains(a));
+
+			if (Console.IsInputRedirected)
+			{
+				string line;
+				while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+				{
+					arguments.Add(line);
+				}
+			}
+
+			List<F> files = new List<F>();
+
+			try
+			{
+				foreach (var arg in arguments)
+				{
+					files.Add(new F{
+						Name = Path.GetFileName(arg).Trim(),
+						FullPath = Path.GetFullPath(arg).Trim()
+					});
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				return;
+			}
+
+			var formatters = new List<string>();
+
+			if (switches.Contains("-f"))
+			{
+				var longest = files.Max(f => f.FullPath.Length);
+				Console.WriteLine("longest f {0}", longest);
+				formatters.Add("{2," + longest + "}");
+			}
+
+			if (switches.Contains("-n"))
+			{
+				var longest = files.Max(f => f.Name.Length);
+				Console.WriteLine("longest n {0}", longest);
+				formatters.Add("{1," + longest + "}");
+			}
+
+			if (switches.Contains("-l"))
+			{
+				formatters.Add("({3})");
+			}
+
+			var formatString = string.Join(" ", formatters);
+
+			if (formatters.Any())
+			{
+				formatString += ": ";
+			}
+
+			formatString += "{0}";
+
+			foreach(var f in files)
+			{
+				using (var stream = new CountingReader(new FileStream(f.FullPath, FileMode.Open, FileAccess.Read)))
+				{
+					string line;
+					while ((line = stream.ReadLine()) != null)
+					{
+						Console.WriteLine(
+							formatString,
+							line,
+							f.Name,
+							f.FullPath,
+							stream.LineNumber);
+					}
+				}
+			}
+		}
+	}
+}
